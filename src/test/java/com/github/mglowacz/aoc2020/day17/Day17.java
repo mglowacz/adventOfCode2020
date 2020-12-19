970d0c2651647a5e7fbd964ff202ec2beeaa8918
mglowacz <mglowacz@omnis.pl>
2020-12-19 22:34:55 +0100

    day17_2

--- src/test/java/com/github/mglowacz/aoc2020/day17/Day17.java
+++ src/test/java/com/github/mglowacz/aoc2020/day17/Day17.java
@@ -14,9 +14,10 @@ public class Day17 {
         char[][][] map = new char[size][size][size];
 
         char[][] map1 = FileInputSource.getMap("/day17/test");
-        for (int j = 0; j < map1.length; j++) {
-            System.arraycopy(map1[j], 0, map[size / 2][j + size / 2 - 1], size / 2 - 1, map1.length);
-        }
+        for (int j = 0; j < map1.length; j++)
+            for (int i = 0; i < map1.length; i++)
+                map[size / 2][j + size / 2 - 1][i + size / 2] = map1[j][i];
+
 
         assertEquals(112, nuberOfActivesAfterCycles(map, 6));
     }
@@ -27,9 +28,9 @@ public class Day17 {
         char[][][] map = new char[size][size][size];
 
         char[][] map1 = FileInputSource.getMap("/day17/data");
-        for (int j = 0; j < map1.length; j++) {
-            System.arraycopy(map1[j], 0, map[size / 2][j + size / 2 - 1], size / 2 - 1, map1.length);
-        }
+        for (int j = 0; j < map1.length; j++)
+            for (int i = 0; i < map1.length; i++)
+                map[size / 2][j + size / 2 - 1][i + size / 2] = map1[j][i];
 
         assertEquals(391, nuberOfActivesAfterCycles(map, 6));
     }
@@ -45,17 +46,9 @@ public class Day17 {
                     for (int i = 0; i < size; i++) {
                         int n = numberOfActiveNeighbors(map, k, j, i);
                         if (map[k][j][i] == '#') {
-                            if (n == 2 || n == 3) {
-                                result[k][j][i] = '#';
-                            } else {
-                                result[k][j][i] = '.';
-                            }
+                            result[k][j][i] = (n == 2 || n == 3) ? '#' : '.';
                         } else {
-                            if (n == 3) {
-                                result[k][j][i] = '#';
-                            } else {
-                                result[k][j][i] = '.';
-                            }
+                            result[k][j][i] = (n == 3) ? '#' : '.';
                         }
                     }
                 }
@@ -93,4 +86,89 @@ public class Day17 {
 
         return n;
     }
+
+
+    @Test
+    public void test2() throws IOException {
+        int size = 20;
+        char[][][][] map = new char[size][size][size][size];
+
+        char[][] map1 = FileInputSource.getMap("/day17/test");
+        for (int j = 0; j < map1.length; j++)
+            for (int i = 0; i < map1.length; i++)
+                map[size / 2][size / 2][j + size / 2 - 1][i + size / 2] = map1[j][i];
+
+        assertEquals(848, nuberOfActivesAfterCycles4(map, 6));
+    }
+
+    @Test
+    public void data2() throws IOException {
+        int size = 30;
+        char[][][][] map = new char[size][size][size][size];
+
+        char[][] map1 = FileInputSource.getMap("/day17/data");
+        for (int j = 0; j < map1.length; j++)
+            for (int i = 0; i < map1.length; i++)
+                map[size / 2][size / 2][j + size / 2 - 1][i + size / 2] = map1[j][i];
+
+        assertEquals(2264, nuberOfActivesAfterCycles4(map, 6));
+    }
+
+    private int nuberOfActivesAfterCycles4(char[][][][] map, int cycles) {
+        int size = map.length;
+        int actives = 0;
+        for (int c = 0; c < cycles; c++) {
+            actives = 0;
+            char[][][][] result = new char[size][size][size][size];
+            for (int m = 0; m < size; m++)
+                for (int k = 0; k < size; k++) {
+                    for (int j = 0; j < size; j++) {
+                        for (int i = 0; i < size; i++) {
+                            int n = numberOfActiveNeighbors4(map, m, k, j, i);
+                            if (map[m][k][j][i] == '#') {
+                                result[m][k][j][i] = (n == 2 || n == 3) ? '#' : '.';
+                            } else {
+                                result[m][k][j][i] = (n == 3) ? '#' : '.';
+                            }
+                        }
+                    }
+                }
+
+            for (int m = 0; m < size; m++)
+                for (int k = 0; k < size; k++) {
+                    for (int j = 0; j < size; j++) {
+                        for (int i = 0; i < size; i++) {
+                            if (result[m][k][j][i] == '#') {
+                                map[m][k][j][i] = '#';
+                                actives++;
+                            } else {
+                                map[m][k][j][i] = '.';
+                            }
+                        }
+                    }
+                }
+        }
+        return actives;
+    }
+
+
+    private int numberOfActiveNeighbors4(char[][][][] map, int m, int k, int j, int i) {
+        int n = 0;
+
+        for (int a = -1; a <= 1; a++)
+            for (int b = -1; b <= 1; b++)
+                for (int c = -1; c <= 1; c++)
+                    for (int d = -1; d <= 1; d++)
+                        if (!(a == 0 && b == 0 && c == 0 && d == 0)
+                                && m + a >= 0 && m + a < map.length
+                                && k + b >= 0 && k + b < map.length
+                                && j + c >= 0 && j + c < map.length
+                                && i + d >= 0 && i + d < map.length
+                                && map[m + a][k + b][j + c][i + d] == '#') n++;
+        return n;
+    }
+
 }
+
+
+
